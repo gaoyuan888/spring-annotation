50a7453c5dd5b1ec9e7ac5545b61870ba4e5801f
yaoyizhou1 <yauri.zhang@qunar.com>
2019-08-07 10:50:53 +0800

    初始

--- src/main/java/com/atguigu/ext/ExtConfig.java
+++ src/main/java/com/atguigu/ext/ExtConfig.java
@@ -19,7 +19,7 @@ import com.atguigu.bean.Blue;
  * 
  * 1、BeanFactoryPostProcessor：beanFactory的后置处理器；
  * 		在BeanFactory标准初始化之后调用，来定制和修改BeanFactory的内容；
- * 		所有的bean定义已经保存加载到beanFactory，但是bean的实例还未创建
+ * 		【标准初始化】所有的bean定义已经保存加载到beanFactory，但是bean的实例还未创建
  * 
  * 
  * BeanFactoryPostProcessor原理:
