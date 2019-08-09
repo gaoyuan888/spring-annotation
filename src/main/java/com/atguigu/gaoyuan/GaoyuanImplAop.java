2bf88ac066f45206837ce2387d4a010301910cbf
yaoyizhou1 <yauri.zhang@qunar.com>
2019-08-09 15:08:14 +0800

    初始

--- src/main/java/com/atguigu/gaoyuan/GaoyuanImplAop.java
+++ src/main/java/com/atguigu/gaoyuan/GaoyuanImplAop.java
@@ -18,6 +18,7 @@ import java.lang.reflect.Proxy;
 public class GaoyuanImplAop implements BeanPostProcessor {
     @Override
     public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
+        //注意，这里所有ioc中的类都会进这里
         //只对指定的bean做代理，返回代理类
         if (beanName.equals("gaoyuan")) {
             Class<?> aClass = bean.getClass().getInterfaces()[0];
